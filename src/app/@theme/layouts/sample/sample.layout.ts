635669c0d7b143998ec04a9f422248d86acb6b10
LoudSoftware <nicoznam@gmail.com>
2018-04-11 19:06:06 -0400

    made link work

--- src/app/@theme/layouts/sample/sample.layout.ts
+++ src/app/@theme/layouts/sample/sample.layout.ts
@@ -29,7 +29,7 @@ import 'rxjs/add/operator/delay';
                    responsive
                    [right]="sidebar.id === 'right'">
         <nb-sidebar-header>
-          <a href="/pages/createanimal" class="btn btn-hero-success main-btn">
+          <a href="#/pages/createanimal" class="btn btn-hero-success main-btn">
             <i class="ion ion-ios-paw"></i> <span>Create Animal</span>
           </a>
         </nb-sidebar-header>
