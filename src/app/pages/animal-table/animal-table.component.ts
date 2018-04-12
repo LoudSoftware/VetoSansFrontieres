c70bc006dbc28ee4ac2cb72e71eed3d4f59ee7da
JeremieBeaudoin-Dion <jeremie.beaudoin.dion@gmail.com>
2018-04-11 20:42:18 -0400

    Added filterrrrrs

--- src/app/pages/animal-table/animal-table.component.ts
+++ src/app/pages/animal-table/animal-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SmartTableService } from '../../@core/data/smart-table.service';
+import { ClinicModel } from '../../@core/data/clinic-model';
 
 @Component(
   {
@@ -19,13 +20,13 @@ import { SmartTableService } from '../../@core/data/smart-table.service';
 export class AnimalTableComponent implements OnInit {
   public data;
 
-
-
+  public clinics: ClinicModel[];
 
   constructor(private service: SmartTableService) { } // Injecting Table Service
 
   ngOnInit() {
     this.getAllAnimals(); // Grab all the animals and save them to the data array
+    this.getAllClinics();
 
   }
 
@@ -42,6 +43,14 @@ export class AnimalTableComponent implements OnInit {
       }
     );
     
+  }
+
+  private getAllClinics() {
+    this.service.getClinics().subscribe(
+      data => this.clinics = data,
+      err => console.log(err),
+      () => console.log("done loading clinics...")
+    );
   }
 
 }
