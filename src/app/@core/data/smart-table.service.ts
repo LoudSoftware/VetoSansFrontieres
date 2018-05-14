775049b2137046a1b571275842c2c87a52c69880
LoudSoftware <nicoznam@gmail.com>
2018-05-14 17:11:05 -0400

    Changed to HttpClient and removed unused testing files

--- src/app/@core/data/smart-table.service.ts
+++ src/app/@core/data/smart-table.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Response, Headers , Http} from '@angular/http';
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 
 
@@ -20,111 +20,80 @@ export class SmartTableService {
   // private apiURL = "http://loud.software:3000/api/";
   private apiURL = "https://vetosansfrontieresbackend.herokuapp.com/api";
 
-  constructor(private _http: Http) { }
+  constructor(private http: HttpClient) { }
 
 
   getAnimals(): Observable<AnimalModel[]> {
-    return this._http
-      .get(`${this.apiURL}/animal`)
-      .map(result => {
-        const animal = result.json()['data'];
-        console.log(animal);
-        return animal.map((animal) => new AnimalModel(animal));
-      });
-  }
-
-  getAnimalTypes(): Observable<any[]> {
-    return this._http
-      .get(`${this.apiURL}/animaltypes`)
-      .map(result => {
-        const animaltype = result.json()['data'];
-        console.log(animaltype);
-        return animaltype;
-      });
+    return this.http
+      .get<AnimalModel[]>(`${this.apiURL}/animal`)
+      .map(result => result['data']);
   }
 
   getAnimal(id: number): Observable<AnimalModel> {
-    return this._http
-      .get(`${this.apiURL}/animal/${id}`)
-      .map(res => new AnimalModel(res.json()['data']));
+    return this.http
+      .get<AnimalModel>(`${this.apiURL}/animal/${id}`)
+      .map(result => result['data']);
   }
 
+  getAnimalTypes(): Observable<any[]> {
+    return this.http
+      .get<any[]>(`${this.apiURL}/animaltypes`)
+      .map(result => result['data']);
+  }
+
+
   updateAnimal(animal: AnimalModel): Observable<AnimalModel> {
-    return this._http
-      .put(`${this.apiURL}/animal/${animal.animalno}`, animal)
-      .map(response => response.json());
+    return this.http
+      .put<AnimalModel>(`${this.apiURL}/animal/${animal.animalno}`, animal);
   }
 
   getPersonnel(): Observable<PersonnelModel[]> {
-    return this._http
-      .get(`${this.apiURL}/allpersonnel`)
-      .map(result => {
-        const personnel = result.json()['data'];
-        console.log(personnel);
-        return personnel.map((personnel) => new PersonnelModel(personnel));
-      });
+    return this.http
+      .get<PersonnelModel>(`${this.apiURL}/allpersonnel`)
+      .map(res => res['data']);
   }
+
 
   getOwners(): Observable<OwnerModel[]> {
-    return this._http
-      .get(`${this.apiURL}/owner`)
-      .map(result => {
-        const owner = result.json()['data'];
-        console.log(owner);
-        return owner.map((owner) => new OwnerModel(owner));
-      });
+    return this.http
+      .get<OwnerModel>(`${this.apiURL}/owner`)
+      .map(res => res['data']);
   }
+
 
   getClinics(): Observable<ClinicModel[]> {
-    return this._http
-      .get(`${this.apiURL}/clinics`)
-      .map(result => {
-        const clinic = result.json()['data'];
-        console.log(clinic);
-        return clinic.map((clinic) => new ClinicModel(clinic));
-      });
+    return this.http
+      .get<ClinicModel>(`${this.apiURL}/clinics`)
+      .map(res => res['data']);
   }
 
+
   getAllTreatments(): Observable<TreatmentsModel[]> {
-    return this._http
-      .get(`${this.apiURL}/treatments`)
-      .map(result => {
-        const treatments = result.json()['data'];
-        console.log(treatments);
-        return treatments.map((treatments) => new TreatmentsModel(treatments));
-      });
+    return this.http
+      .get<TreatmentsModel[]>(`${this.apiURL}/treatments`)
+      .map(result => result['data']);
   }
 
   getTreatment(id: number): Observable<TreatmentModel[]> {
-    return this._http
-      .get(`${this.apiURL}/treatment/${id}`)
-      .map(result => {
-        const treatments = result.json()['data'];
-        console.log(treatments);
-        return treatments.map((treatments) => new TreatmentModel(treatments));
-      });
+    return this.http
+      .get<TreatmentModel[]>(`${this.apiURL}/treatment/${id}`)
+      .map(result => result['data']);
   }
 
   getExamen(id: number): Observable<ExamenModel[]> {
-    return this._http
-      .get(`${this.apiURL}/examination/${id}`)
-      .map(result => {
-        const examen = result.json()['data'];
-        console.log(examen);
-        return examen.map((examen) => new ExamenModel(examen));
-      });
+    return this.http
+      .get<ExamenModel>(`${this.apiURL}/examination/${id}`)
+      .map(result => result['data']);
   }
 
-  public removeAnimal(animal: AnimalModel): Observable<null> {
-    return this._http
-      .delete(`${this.apiURL}/animal/${animal.animalno}`)
-      .map(response => response.json());
+  public removeAnimal(animal: AnimalModel): Observable<AnimalModel> {
+    return this.http
+      .delete<AnimalModel>(`${this.apiURL}/animal/${animal.animalno}`);
   }
 
   createAnimal(animal: AnimalModel) {
-    return this._http
-      .post(`${this.apiURL}/animal/`, animal)
-      .map(response => response.json());
+    return this.http
+      .post<AnimalModel>(`${this.apiURL}/animal/`, animal);
   }
 
 }
